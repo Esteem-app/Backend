2914ace354daaf28359b2453dae5fda8f82436d7
maria <maria77julia@gmail.com>
2021-05-28 22:06:33 +0200

    typo

--- backend/settings.py
+++ backend/settings.py
@@ -155,5 +155,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-print(db_form_env)
+print(db_from_env)
 DATABASES['default'].update(db_from_env)
