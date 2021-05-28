a052b59448e5c7b504a35f4381aa5df7251f260d
maria <maria77julia@gmail.com>
2021-05-28 11:16:27 +0200

    first project setup

---
+++ example_settings.py
@@ -0,0 +1,13 @@
+SECRET_KEY = '<secret key>'
+
+
+DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'esteem',
+        'HOST': '<hostname>',
+        'PASSWORD': '<password>',
+        'USER': '<db username>',
+        'PORT': '<port>',
+        }
+   }        
