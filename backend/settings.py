3dcbcdd94da40b6ce5fe36f19c48b15f4723fc68
maria <maria77julia@gmail.com>
2021-05-28 14:46:09 +0200

    user model and user admin

--- backend/settings.py
+++ backend/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = 'users.MyUser'
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
